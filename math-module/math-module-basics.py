a825557cf786735c91d857331f1c6b732301b361
LukaszZagaja <103201568+LukaszZagaja@users.noreply.github.com>
2023-12-21 09:55:14 +0100

    math_module

---
+++ math-module/math-module-basics.py
@@ -0,0 +1,30 @@
+import math
+
+#help(math)
+value = 4.35
+
+print("\n")
+print("Value of our variable and then rounding with math module")
+print(value)
+print(math.floor(value))
+print(math.ceil(value))
+print("\n")
+
+print("Python rounding")
+print(round(value))
+print(round(4.5))
+print(round(5.5))
+print("\n")
+
+print("Math: pi, e, inf, nan")
+print(math.pi)
+print(math.e)
+print(math.inf)
+print(math.nan)
+print("\n")
+
+print("Math: log, sin, radians to degrees")
+print(math.log(math.e))
+print(math.sin(10))
+print(math.degrees(math.pi/2))
+print(math.radians(180))
