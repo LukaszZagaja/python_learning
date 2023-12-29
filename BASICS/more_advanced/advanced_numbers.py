53136185ae5672f208f3fb7b0b5000d1d066217f
LukaszZagaja <103201568+LukaszZagaja@users.noreply.github.com>
2023-12-29 16:27:09 +0100

    advanced_numbers

---
+++ BASICS/more_advanced/advanced_numbers.py
@@ -0,0 +1,29 @@
+print('\n')
+print('Hexadecimal numbers') 
+print(hex(112))
+print(hex(256))
+print('\n')
+
+print('Binary numbers')
+print(bin(10))
+print(bin(128))
+print('\n')
+
+print('Power')
+print(pow(2,4)) # 2 to the power of 4
+#print(2**4)     # the same thing
+print(pow(2,4,3)) # (2 to the power of 4) % 3 = 1
+print('\n')
+
+print('Absolute ')
+print(abs(-3))
+print(abs(12))
+print('\n')
+
+print('Rounding')
+print(round(3.1))
+print(round(3.7))
+# print(type(round(3.7))) its INT
+print(round(3))
+print(round(3.14159, 2)) # rounding to 2 places past 
+
