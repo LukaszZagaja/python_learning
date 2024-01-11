63e340768d55ac4a7b40c77014f3f541100f9b84
LukaszZagaja <103201568+LukaszZagaja@users.noreply.github.com>
2024-01-11 21:13:04 +0100

    two_dimensional_arrays

---
+++ Learning/DSA_datastructures_and_algorithms/arrays/numpy_module/TwoDimensionalArray.py
@@ -0,0 +1,9 @@
+import numpy as numpy
+
+# day 1: 11, 15, 10, 6
+# day 2: 10, 14, 11, 5
+# day 3: 12, 17, 12, 8
+# day 4: 15, 18, 14, 9
+
+two_dim_arr = numpy.array([[11, 15, 10, 6], [10, 14, 11, 5], [12, 17, 12, 8], [15, 18, 14, 9]])
+print(two_dim_arr)
