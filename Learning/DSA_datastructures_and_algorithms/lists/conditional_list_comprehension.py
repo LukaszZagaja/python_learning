7a3a2f24f8b5dd8181c782e2d9100c8e29d6cbf0
LukaszZagaja <103201568+LukaszZagaja@users.noreply.github.com>
2024-01-17 15:50:19 +0100

    conditional_list_comprehension

---
+++ Learning/DSA_datastructures_and_algorithms/lists/conditional_list_comprehension.py
@@ -0,0 +1,8 @@
+list1 = [-1, 2, -3, 4, -5, 6, -7, 8, -9, 10]
+new_list = [number**2 for number in list1 if number < 0]
+
+print(f'Original list:                           {list1}')
+print(f'Numbers smaller than 0 to the root of 2: {new_list}')
+
+newer_list = [number**2 if number > 0 else 0 for number in list1]
+print(f'List comprehension with if:              {newer_list}')
