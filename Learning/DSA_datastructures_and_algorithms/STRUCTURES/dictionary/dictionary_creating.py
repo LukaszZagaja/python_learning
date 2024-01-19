510da108a77324c26b2094b0f4cca1a86f4db609
LukaszZagaja <103201568+LukaszZagaja@users.noreply.github.com>
2024-01-19 16:46:57 +0100

    dictionary creating

---
+++ Learning/DSA_datastructures_and_algorithms/STRUCTURES/dictionary/dictionary_creating.py
@@ -0,0 +1,15 @@
+my_dict = dict()
+print(my_dict)
+my_dict2 = {}
+print(my_dict2)
+
+eng_pl = dict(one='jeden', two='dwa', three='trzy')
+print(eng_pl)
+
+eng_de = {'one':"eins", 'two':'zwei', 'three':'drei'}
+print(eng_de)
+
+print('\nFrom tuples:')
+eng_fr_tuple = [('one', 'une'), ('two', 'deux'), ('three', 'trois')]
+eng_fr = dict(eng_fr_tuple)
+print(eng_fr)
