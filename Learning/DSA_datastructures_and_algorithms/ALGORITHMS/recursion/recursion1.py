6ee710450d43e39747792ec49fd3309d6ec72dea
LukaszZagaja <103201568+LukaszZagaja@users.noreply.github.com>
2024-01-24 20:40:05 +0100

    recursive_2

--- Learning/DSA_datastructures_and_algorithms/ALGORITHMS/recursion/recursion1.py
+++ Learning/DSA_datastructures_and_algorithms/ALGORITHMS/recursion/recursion1.py
@@ -6,3 +6,13 @@ def matryoshka(doll):
 
 
 matryoshka(10)
+
+def recursiveMethod(n):
+    if n<1:
+        print('n is smaller than 1')
+    else:
+        #print(n)
+        recursiveMethod(n-1)
+        print(n)
+
+recursiveMethod(5)
