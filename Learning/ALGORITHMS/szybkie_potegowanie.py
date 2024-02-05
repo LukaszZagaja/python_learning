ad4ce6a9e5e2dbbd2ce903eba373711cef89a26b
LukaszZagaja <103201568+LukaszZagaja@users.noreply.github.com>
2024-02-05 18:45:35 +0100

    szybkie mnozenie

--- Learning/ALGORITHMS/szybkie_potegowanie.py
+++ Learning/ALGORITHMS/szybkie_potegowanie.py
@@ -3,8 +3,15 @@ def potegowanieSzybkie(podstawa,wykladnik):
         return 1
     wynik = 1
     while wykladnik > 0:
-        if wykladnik%2==1: # if there is 1 then both if 0 then only podstawa
+        if wykladnik%2==1: # 10100110 <- bits. if we meet 1 then both if 0 then only podstawa
             wynik*=podstawa
+        print(f'Podstawa przed: {podstawa}')
         podstawa*=podstawa
+        print(f'Podstawa po mnozeniu: {podstawa}')
+        print(f'Wykladnik przed: {wykladnik}')
         wykladnik = wykladnik//2
+        print(f'Wykladnik po dzieleniu: {wykladnik}\n')
     return wynik
+
+
+potegowanieSzybkie(2,5)
